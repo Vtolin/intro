8bb7a30d0ae6d1cc09222aed748cea1464c5afc2
Rajendra Pasha <105064715+Vtolin@users.noreply.github.com>
2025-06-12 15:00:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ anchor.addEventListener('click', function (e) {
 });
 
 document.querySelector('.contact-form').addEventListener('submit', function(e) {
-            e.preventDefault();
+            e.preventDefault()
+            const form = this
 
             let formData = {
                 name: document.getElementById('name').value,
@@ -16,7 +17,7 @@ document.querySelector('.contact-form').addEventListener('submit', function(e) {
                 message: document.getElementById('message').value
             };
 
-            fetch(this.action, {
+            fetch(form.action, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -26,7 +27,7 @@ document.querySelector('.contact-form').addEventListener('submit', function(e) {
             }).then(response => {
                 if (response.ok) {
                     alert('Thank you! Your message has been sent.');
-                    this.reset();
+                    form.reset();
                 } else {
                     alert('Oops! Something went wrong. Please try again.');
                 }
